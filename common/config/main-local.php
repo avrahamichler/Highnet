c592191b228953fdd12840d413329d13d53e7d70
avrumi <avrumi@ravtech.co.il>
2016-01-24 12:48:29 +0200

    change path to db

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,9 +1,18 @@
 <?php
+if( __DIR__ == "C:\\Program Files (x86)\\EasyPHP-Devserver-16.1\\eds-www\\Highnet\\common\\config")
+{
+    $path = 'pgsql:host=192.168.44.55;port=5432;dbname=sns';
+}
+else
+{
+    $path =  'pgsql:host=94.188.161.142;port=5432;dbname=sns';
+}
+
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=192.168.44.55;port=5432;dbname=sns',
+            'dsn' => $path,
             'username' => 'snspp',
             'password' => 'highnet',
             'charset' => 'utf8',
